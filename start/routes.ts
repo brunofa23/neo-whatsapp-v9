fb7b925cffb73d02b16fe988a06f0c02775bd500
Bruno Favato <brunofa23@gmail.com>
2023-07-10 14:58:36 -0300

    ok

--- start/routes.ts
+++ start/routes.ts
@@ -1,14 +1,20 @@
 import Route from '@ioc:Adonis/Core/Route'
 import Database from '@ioc:Adonis/Lucid/Database'
 
+
+
 Route.get('/', async () => {
   return { hello: 'world' }
 })
 
 Route.get('/teste', async () => {
-  //return "tester"
-  console.log(Database.from('teste').select('*').toSQL())
-  return Database.from('teste').select('*')
-  //return Database.from('posts').select('*')
+
+  return Database.connection('mssql').from('teste').select('*')
+})
+
+
+Route.get('/mysql', async () => {
+  return Database.connection('mysql').from('emp').select('*')
+
 })
 

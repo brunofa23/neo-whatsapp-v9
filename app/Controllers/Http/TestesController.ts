419d48936a8cc878ed52a6972aeb7d5c08422319
Bruno Favato do Amaral <brunofa23@gmail.com>
2023-07-13 13:26:06 -0300

    ok

--- app/Controllers/Http/TestesController.ts
+++ app/Controllers/Http/TestesController.ts
@@ -4,6 +4,6 @@ import Database from '@ioc:Adonis/Lucid/Database'
 //const database = require('config/database')
 export default class TestesController {
 
-  const users = await Database.connection('mysql').table('emp')
+  const users = await Database.connection('mysql').table('teste')
 
 }
--- start/routes.ts
+++ start/routes.ts
@@ -1,14 +1,13 @@
 import Route from '@ioc:Adonis/Core/Route'
 import Database from '@ioc:Adonis/Lucid/Database'
 
-
-
 Route.get('/', async () => {
   return { hello: 'world' }
 })
 
 Route.get('/teste', async () => {
-
+  const result = Database.connection('mssql').from('teste').select('*')
+  console.log("entrei aqui", await Database.connection('mssql').from('teste').select('*'))
   return Database.connection('mssql').from('teste').select('*')
 })
 

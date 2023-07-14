182a9c3a5a688011cb06487a529e448f8fb6e0ba
Bruno Favato do Amaral <brunofa23@gmail.com>
2023-07-14 18:02:11 -0300

    ajustado!!!

--- start/routes.ts
+++ start/routes.ts
@@ -12,9 +12,11 @@ Route.group(() => {
   // Route.get('/teste1', "TestesControler.teste")
 
   Route.get('/teste', async () => {
+    const teste = require('../../neo-whatsapp-v9/app/Services/whatsapp-web/teste.ts')
+    return teste.listPac()
     //const result = Database.connection('mssql').from('teste').select('*')
     //console.log("entrei aqui", await Database.connection('mssql').from('pac').select('*'))
-    return (await Database.connection('mssql').from('pac').select('*').where('pac_reg', '=', '15'))
+    //return (await Database.connection('mssql').from('pac').select('*').where('pac_reg', '=', '15'))
   })
 
 }).prefix('/api')

ab379199240c691f30ee81943b27b643a1fec367
Bruno Favato do Amaral <brunofa23@gmail.com>
2023-07-17 14:26:44 -0300

    ok

--- app/Services/whatsapp-web/SendRepeatedMessage.ts
+++ app/Services/whatsapp-web/SendRepeatedMessage.ts
@@ -1,11 +1,24 @@
-import { scheduledPatients } from '../../Controllers/Http/ShippingcampaignsController old'
+import { Shipping } from 'App/Controllers/Http/TestesController';
 import { verifyNumber } from '../../Services/whatsapp-web/VerifyNumber'
-
+import ShippingcampaignsController from 'App/Controllers/Http/ShippingcampaignsController';
+import shipping from 'App/Controllers/Http/TestesController'
 
 async function sendRepeatedMessage(client) {
 
-  const shipping = await scheduledPatients()
+  //const shipping = await require('../../Controllers/Http/TestesController')
+  //console.log("SHIPPING:::", await shipping.Shipping())
 
+  //const shipping = require('../../Controllers/Http/TestesController')
+  try {
+    await shipping.store()
+    console.log("gravado com sucesso")
+  } catch (error) {
+    console.log("erorrrrr:", error)
+  }
+
+
+  //const shipping1 = require('../../Models/Shippingcampaign')
+  //console.log("SHIPPING 111:::", await shipping1.all())
 
   //1 - Buscar os pacientes dos filtros selecionados
   //const shippingCampaigns = await require('../../Controllers/Http/ShippingcampaignsController')

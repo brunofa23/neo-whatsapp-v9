7365c78987e34be0622eefc33d7dd523da1e7a04
Bruno Favato do Amaral <brunofa23@gmail.com>
2023-07-19 23:32:02 -0300

    reencaminhamento

--- app/Services/whatsapp-web/ChatMonitoring.ts
+++ app/Services/whatsapp-web/ChatMonitoring.ts
@@ -1,5 +1,7 @@
+import { verifyNumber } from '../../Services/whatsapp-web/VerifyNumber'
 import { Client } from 'whatsapp-web.js';
 import Chat from 'App/Models/Chat';
+
 
 export default class Monitoring {
 
@@ -11,6 +13,7 @@ export default class Monitoring {
 
         //const chat = await Chat.findBy('cellphoneserialized', message.from)
         const chat = await Chat.query().where('cellphoneserialized', '=', message.from).whereNull('response').first()
+        let chat2 = new Chat()
         if (chat) {
           if (chat.interaction == 1) {//confirmação de agenda
             if (message.body == 'Sim')//presença confirmada
@@ -27,27 +30,41 @@ export default class Monitoring {
                 chat.save()
 
                 client.sendMessage(message.from, "Gostaria de reagendar para outro horário? \n1-Sim \n2-Não")
-                let chat2 = new Chat()
-                //chat2 = chat
+
                 chat2.interaction = 2
                 chat2.name = chat.name
                 chat2.cellphone = chat.cellphone
-                chat.cellphoneserialized = message.from
+                chat2.cellphoneserialized = message.from
                 chat2.message = "Gostaria de reagendar para outro horário? \n1-Sim \n2-Não"
                 await Chat.create(chat2)
               }
           } if (chat.interaction == 2) {
             if (message.body == "Sim") {
               client.sendMessage(message.from, "Tudo bem, estamos encaminhando sua chamada.")
+              //const numeroDestino = '5531990691174';
+              const numeroDestino = await verifyNumber(client, '5531990691174')
+              console.log("NUMERO DESTINO", numeroDestino)
+              const mensagem = `Olá ${chat.name} para quando gostaria de reagendar a consulta?`;
+              const linkRedirecionamento = `https://api.whatsapp.com/send?phone=${chat.cellphone}&text=${encodeURIComponent(mensagem)}`;
+
+              //client.sendMessage(numeroDestino, "Segue o link para reagendamento de Consulta...")
+              client.sendMessage(numeroDestino, `${linkRedirecionamento}`).then(() => {
+                console.log('Mensagem enviada!');
+              }).catch((erro) => {
+                console.error('Erro ao enviar a mensagem:', erro);
+              });
+
               //gerar um link e enviar para um determinado numero da atendente
             } else if (message.body == "Não") {
               client.sendMessage(message.from, "Tudo bem, iremos encerrar a conversa. A Neo agradece.")
+              chat2.message = message.body
+              chat.save()
             }
 
           }
         }
 
-        console.log("MENSAGEM::", message.body, "numero", chat)
+        //console.log("MENSAGEM::", message.body, "numero", chat)
 
 
 

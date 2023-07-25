2678d416f36d59034b2ff5f48a091386b032bf52
Bruno Favato do Amaral <brunofa23@gmail.com>
2023-07-24 23:49:01 -0300

    testes confirmados

--- app/Services/whatsapp-web/SendMessage.ts
+++ app/Services/whatsapp-web/SendMessage.ts
@@ -2,38 +2,45 @@ import Chat from "App/Models/Chat"
 import Shippingcampaign from "App/Models/Shippingcampaign"
 import { Client } from "whatsapp-web.js"
 
+
 export default async (client: Client, shippingCampaignList: Shippingcampaign[]) => {
 
-  for (const dataRow of shippingCampaignList) {
-    try {
-      if (dataRow.phonevalid && !dataRow.messagesent) {
-        //const send = await client.sendMessage(dataRow.cellphoneserialized, dataRow.message)
-        await client.sendMessage(dataRow.cellphoneserialized, dataRow.message)
-          .then((response) => {
-            dataRow.messagesent = true
-            dataRow.save()
-          }).catch((error) => {
-            console.log("ERRRRO:::", error)
-          })
+  async function sendMessages() {
+    for (const dataRow of shippingCampaignList) {
+      try {
+        await new Promise(resolve => setTimeout(resolve, 10000));
+        if (dataRow.phonevalid && !dataRow.messagesent) {
+          //const send = await client.sendMessage(dataRow.cellphoneserialized, dataRow.message)
+          await client.sendMessage(dataRow.cellphoneserialized, dataRow.message)
+            .then((response) => {
+              dataRow.messagesent = true
+              dataRow.save()
+            }).catch((error) => {
+              console.log("ERRRRO:::", error)
+            })
 
-        const bodyChat = {
-          interaction_id: dataRow.interaction_id,
-          interaction_seq: dataRow.interaction_seq,
-          idexternal: dataRow.idexternal,
-          reg: dataRow.reg,
-          name: dataRow.name,
-          cellphone: dataRow.cellphone,
-          cellphoneserialized: dataRow.cellphoneserialized,
-          message: dataRow.message,
-          shippingcampaigns_id: dataRow.id
+          const bodyChat = {
+            interaction_id: dataRow.interaction_id,
+            interaction_seq: dataRow.interaction_seq,
+            idexternal: dataRow.idexternal,
+            reg: dataRow.reg,
+            name: dataRow.name,
+            cellphone: dataRow.cellphone,
+            cellphoneserialized: dataRow.cellphoneserialized,
+            message: dataRow.message,
+            shippingcampaigns_id: dataRow.id
+          }
+          await Chat.create(bodyChat)
+
         }
-        await Chat.create(bodyChat)
-
+      } catch (error) {
+        console.log("ERRO:::", error)
       }
-    } catch (error) {
-      console.log("ERRO:::", error)
     }
   }
+
+  await sendMessages()
+
 
 
 

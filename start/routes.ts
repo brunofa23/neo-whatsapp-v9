918bc3b592863e1517f58c38b36503845c00b74a
Bruno Favato <brunofa23@gmail.com>
2023-07-29 17:21:42 -0300

    v-26

--- start/routes.ts
+++ start/routes.ts
@@ -6,7 +6,7 @@ import { executeWhatsapp } from '../app/Services/whatsapp-web/whatsapp'
 //import { sendRepeatedMessage } from '../app/Services/whatsapp-web/SendRepeatedMessage'
 
 Route.get('/', async () => {
-  return { hello: 'world - v25' }
+  return { hello: 'world - v26' }
 })
 
 Route.group(() => {

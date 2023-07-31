849d13ea065099fe7bcd73aa4cc76164a51f2d32
Bruno Favato <brunofa23@gmail.com>
2023-07-31 18:37:01 -0300

    v-11

--- app/Services/whatsapp-web/whatsapp.ts
+++ app/Services/whatsapp-web/whatsapp.ts
@@ -8,7 +8,7 @@ async function executeWhatsapp(logout: boolean = false) {
   const { Client, LocalAuth } = require('whatsapp-web.js');
   const qrcode = require('qrcode-terminal');
 
-  console.log("***CHAT BOT V-10***")
+  console.log("***CHAT BOT V-11***")
 
 
   const client = new Client({

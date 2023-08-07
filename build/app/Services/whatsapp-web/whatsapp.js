15ed391bdf9e95841f5caa4e61fe274592fc953d
Bruno Favato <brunofa23@gmail.com>
2023-08-07 17:47:47 +0000

    nova versão

--- build/app/Services/whatsapp-web/whatsapp.js
+++ build/app/Services/whatsapp-web/whatsapp.js
@@ -3,11 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const ChatMonitoring_1 = __importDefault(require("./ChatMonitoring/ChatMonitoring"));
+const util_1 = require("./util");
 const SendMessage_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Services/whatsapp-web/SendMessage"));
 const SendRepeatedMessage_1 = global[Symbol.for('ioc.use')]("App/Services/whatsapp-web/SendRepeatedMessage");
 const luxon_1 = require("luxon");
-const ChatMonitoring_1 = __importDefault(require("./ChatMonitoring/ChatMonitoring"));
-const util_1 = require("./util");
 async function executeWhatsapp() {
     const { Client, LocalAuth } = require('whatsapp-web.js');
     const qrcode = require('qrcode-terminal');
@@ -41,16 +41,20 @@ async function executeWhatsapp() {
     client.on('ready', async () => {
         console.log('Lendo na Inicialização!');
         const hourSend = await (0, util_1.DateFormat)("HH", luxon_1.DateTime.local());
+        const startTimeSendMessageRepeated = parseInt(process.env.EXECUTE_SEND_REPEATED_MESSAGE);
+        const endtTimeSendMessageRepeated = parseInt(process.env.EXECUTE_SEND_REPEATED_MESSAGE_END);
+        const startTimeSendMessage = parseInt(process.env.EXECUTE_SEND_MESSAGE);
+        const endTimeSendMessage = parseInt(process.env.EXECUTE_SEND_MESSAGE_END);
         if (parseInt(hourSend) > 7 && parseInt(hourSend) < 20) {
             setInterval(async () => {
                 await (0, SendRepeatedMessage_1.sendRepeatedMessage)(client);
-            }, await (0, util_1.GenerateRandomTime)(25, 30));
+            }, await (0, util_1.GenerateRandomTime)(startTimeSendMessageRepeated, endtTimeSendMessageRepeated, '****Send Message Repeated'));
             setInterval(async () => {
                 console.log("Executando ENVIO DE MENSAGEM...");
                 if (!global.executingSendMessage) {
                     await (0, SendMessage_1.default)(client);
                 }
-            }, await (0, util_1.GenerateRandomTime)(18, 20));
+            }, await (0, util_1.GenerateRandomTime)(startTimeSendMessage, endTimeSendMessage, '----Time Send Message'));
         }
     });
     client.on('disconnected', (reason) => {

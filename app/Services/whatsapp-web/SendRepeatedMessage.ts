7e3c6601761739bdcde22d2811d1bb921eff22f9
Bruno Favato <brunofa23@gmail.com>
2023-08-11 18:20:34 -0300

    ajustes

--- app/Services/whatsapp-web/SendRepeatedMessage.ts
+++ app/Services/whatsapp-web/SendRepeatedMessage.ts
@@ -1,22 +1,20 @@
 import { DateTime } from 'luxon';
-import { Client } from 'whatsapp-web.js';
 
 import PersistShippingcampaign from './PersistShippingcampaign';
-import { DateFormat, TimeSchedule } from './util'
+import { DateFormat, GenerateRandomTime, TimeSchedule } from './util'
 
 async function sendRepeatedMessage() {
-  const date = await DateFormat("dd/MM/yyyy HH:mm:ss", DateTime.local())
   const startTimeSendMessageRepeated = parseInt(process.env.EXECUTE_SEND_REPEATED_MESSAGE)
   const endtTimeSendMessageRepeated = parseInt(process.env.EXECUTE_SEND_REPEATED_MESSAGE_END)
-
-
-  if (!global.executingSendMessage) {
-    if (await TimeSchedule()) {
-      console.log(`Buscando dados no Smart: ${date}`)
-      await PersistShippingcampaign()
+  setInterval(async () => {
+    const date = await DateFormat("dd/MM/yyyy HH:mm:ss", DateTime.local())
+    if (!global.executingSendMessage) {
+      if (await TimeSchedule()) {
+        console.log(`Buscando dados no Smart: ${date}`)
+        await PersistShippingcampaign()
+      }
     }
-  }
-
+  }, await GenerateRandomTime(startTimeSendMessageRepeated, endtTimeSendMessageRepeated, '****Send Message Repeated'))
 
 }
 module.exports = { sendRepeatedMessage }

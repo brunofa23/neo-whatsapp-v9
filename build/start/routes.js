6864833af6a0f6c418e88835a6224ca7a00e6c9f
Bruno Favato <brunofa23@gmail.com>
2023-08-15 13:51:26 +0000

    nova versão

--- build/start/routes.js
+++ build/start/routes.js
@@ -3,9 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const whatsapp_1 = require("../app/Services/whatsapp-web/whatsapp");
 const Route_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Route"));
-console.log("***CHAT BOT V-28***");
+const whatsapp_1 = require("../app/Services/whatsapp-web/whatsapp");
+console.log("***CHAT BOT V-29***");
 (0, whatsapp_1.executeWhatsapp)();
 Route_1.default.get('/', async () => {
     return { hello: 'world - v28' };

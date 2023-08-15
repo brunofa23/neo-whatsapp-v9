0f1c1b8d67b53edc1d3e13dc0c189900738f2581
Bruno Favato <brunofa23@gmail.com>
2023-08-15 09:05:17 -0300

    v-28

--- start/routes.ts
+++ start/routes.ts
@@ -1,11 +1,11 @@
 import { executeWhatsapp } from '../app/Services/whatsapp-web/whatsapp'
 import Route from '@ioc:Adonis/Core/Route'
 
-console.log("***CHAT BOT V-27***")
+console.log("***CHAT BOT V-28***")
 executeWhatsapp()
 
 Route.get('/', async () => {
-  return { hello: 'world - v39' }
+  return { hello: 'world - v28' }
 })
 
 Route.group(() => {

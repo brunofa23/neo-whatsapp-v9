f3e7e5f7411d22d76c8e8b95d47428554d440ff0
Bruno Favato <brunofa23@gmail.com>
2023-08-18 14:55:59 -0300

    v-34

--- start/routes.ts
+++ start/routes.ts
@@ -2,7 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 import { executeWhatsapp } from '../app/Services/whatsapp-web/whatsapp'
 
-console.log("***CHAT BOT V-33***")
+console.log("***CHAT BOT V-34***")
 console.log(`***NOME DO CLIENTE: ${process.env.CHAT_NAME}***`)
 executeWhatsapp()
 

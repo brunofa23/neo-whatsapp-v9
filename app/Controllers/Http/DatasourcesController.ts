30198b490584bc5575162115132b2a9fa6940a73
Bruno Favato <brunofa23@gmail.com>
2023-09-14 16:06:53 -0300

    inclusão avaliação de pacientes

--- app/Controllers/Http/DatasourcesController.ts
+++ app/Controllers/Http/DatasourcesController.ts
@@ -17,15 +17,15 @@ export default class DatasourcesController {
     //console.log("queryList:::", interactionList)
     for (const interaction of interactionList) {
       if (interaction.id == 1) {
-        //console.log("CONFIRMAÇÃO DE AGENDAS", interaction.name)
         await Database.manager.close('mssql')
         return await this.scheduledPatients()
       } else
         if (interaction.id == 2) {
-          console.log("Teste de envio amadurecimento do chip", interaction.name)
+          console.log("AVALIAÇÃO DOS PACIENTES", interaction.name)
         }
       if (interaction.id == 3) {
-        console.log("AVALIAÇÃO DOS PACIENTES", interaction.name)
+        console.log("Teste de envio amadurecimento do chip", interaction.name)
+
       }
 
     }
@@ -35,13 +35,11 @@ export default class DatasourcesController {
   }
 
   async scheduledPatients() {
-
     async function greeting(message: String) {
       const greeting = ['Olá!', 'Oi tudo bem?', 'Saudações!', 'Oi como vai?']
       const presentation = ['Eu me chamo Iris', 'Eu sou a Iris', 'Aqui é a Iris']
       return message.replace('{greeting}', greeting[Math.floor(Math.random() * greeting.length)]).replace('{presentation}', presentation[Math.floor(Math.random() * presentation.length)])
     }
-
     const pacQueryModel = await Interaction.find(1)
     const env = process.env.NODE_ENV
     let pacQuery
@@ -58,15 +56,11 @@ export default class DatasourcesController {
       }
       //console.log("RESULTADO", result)
       await Database.manager.close('mssql')
-
       return result
     } catch (error) {
       return { "ERRO": "ERRO 154212", error }
     }
   }
-
-
-
   async confirmSchedule(chat: Chat, chatOtherFields: String = "") {
 
     const dateNow = await DateFormat("dd/MM/yyyy HH:mm:ss", DateTime.local())
@@ -94,10 +88,6 @@ export default class DatasourcesController {
       return error
     }
   }
-
-
-
-
   async cancelSchedule(chat: Chat, chatOtherFields: String = "") {
     const dateNow = await DateFormat("dd/MM/yyyy HH:mm:ss", DateTime.local())
     const dateSchedule = DateTime.fromFormat(chatOtherFields['schedule'], 'yyyy-MM-dd HH:mm')//converte string para data
@@ -125,6 +115,40 @@ export default class DatasourcesController {
       return error
     }
   }
+
+
+  async serviceEvaluation() {
+
+    console.log("AVALIAÇÃO DE PACIENTES...")
+
+    async function greeting(message: String) {
+      const greeting = ['Olá!', 'Oi tudo bem?', 'Saudações!', 'Oi como vai?']
+      const presentation = ['Eu me chamo Iris', 'Eu sou a Iris', 'Aqui é a Iris']
+      return message.replace('{greeting}', greeting[Math.floor(Math.random() * greeting.length)]).replace('{presentation}', presentation[Math.floor(Math.random() * presentation.length)])
+    }
+    const pacQueryModel = await Interaction.find(2)
+    const env = process.env.NODE_ENV
+    let pacQuery
+
+    if (env === 'development')
+      pacQuery = pacQueryModel?.querydev
+    else pacQuery = pacQueryModel?.query
+
+    try {
+      const result = await Database.connection('mssql').rawQuery(pacQuery)
+      for (const data of result) {
+        const message = await greeting(data.message)
+        data.message = message
+      }
+      //console.log("RESULTADO", result)
+      await Database.manager.close('mssql')
+      return result
+    } catch (error) {
+      return { "ERRO": "ERRO 21221", error }
+    }
+
+  }
+
 
 
 

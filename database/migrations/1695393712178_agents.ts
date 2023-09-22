b6eede0ed6b5c7aaf5f3c5a1f06902b1c27d1de4
Bruno Favato <brunofa23@gmail.com>
2023-09-22 16:45:19 -0300

    teste

--- database/migrations/1695393712178_agents.ts
+++ database/migrations/1695393712178_agents.ts
@@ -7,19 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id').primary().notNullable()
       table.string('name').notNullable()
-      table.string('number_phone')
-      table.integer('interval_init_query').notNullable()
-      table.integer('interval_final_query').notNullable()
-      table.integer('interval_init_message').notNullable()
-      table.integer('interval_final_message').notNullable()
-      table.integer('max_limit_message').notNullable()
-      table.string('status')
-      table.boolean('active').defaultTo('true')
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
-      table.timestamp('created_at', { useTz: true })
-      table.timestamp('updated_at', { useTz: true })
+
     })
   }
 
@@ -27,3 +15,5 @@ export default class extends BaseSchema {
     this.schema.dropTable(this.tableName)
   }
 }
+
+

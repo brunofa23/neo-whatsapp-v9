4fa5a831d11e32f86b056965185d1bc1fd31fa63
Bruno Favato <brunofa23@gmail.com>
2023-12-04 14:56:44 -0300

    ok

--- app/Models/Agent.ts
+++ app/Models/Agent.ts
@@ -1,7 +1,17 @@
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
 
+import Env from '@ioc:Adonis/Core/Env'
+
 export default class Agent extends BaseModel {
+
+
+  static get connection() {
+    //return 'mysql';
+    return Env.get('DB_CONNECTION_MAIN')
+  }
+
+
   @column({ isPrimary: true })
   public id: number
 
--- app/Models/Interaction.ts
+++ app/Models/Interaction.ts
@@ -1,7 +1,16 @@
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
 import { DateTime } from 'luxon'
 
+import Env from '@ioc:Adonis/Core/Env'
+
 export default class Interaction extends BaseModel {
+
+  static get connection() {
+    //return 'mysql';
+    return Env.get('DB_CONNECTION_MAIN')
+  }
+
+
   @column({ isPrimary: true })
   public id: number
 

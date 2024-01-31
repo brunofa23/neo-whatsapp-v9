ebae19fd53ce3f8a6c4cb5a538c07eef2e2265db
Bruno Favato <brunofa23@gmail.com>
2024-01-31 13:48:15 -0300

    ok

--- app/Controllers/Http/CustomchatsController.ts
+++ app/Controllers/Http/CustomchatsController.ts
@@ -3,17 +3,17 @@ import Customchat from 'App/Models/Customchat'
 
 export default class CustomchatsController {
 
-
   public async show({ auth, params, response }: HttpContextContract) {
     //const authenticate = await auth.use('api').authenticate()
     const data = await Customchat.query().where('chats_id', params.id)
     return response.status(200).send(data)
   }
 
-  public async sendMessage({ params, request, response }: HttpContextContract) {
+  public async sendMessage({ request, response }: HttpContextContract) {
+
     const body = request.only(Customchat.fillable)
     body.messagesent = false
-    //console.log("body", body)
+    console.log("Passei aqui 45888", body)
     try {
       const payLoad = await Customchat.create(body)
       return response.status(201).send(payLoad)

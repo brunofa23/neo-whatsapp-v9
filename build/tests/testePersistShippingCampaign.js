f7947664878b12bfab2f1083fc53ff604f900d17
Bruno Favato <brunofa23@gmail.com>
2024-03-10 22:46:44 -0300

    ok

--- build/tests/testePersistShippingCampaign.js
+++
@@ -1,7 +0,0 @@
-async function temporizadorComSegundosRandomicos() {
-    const value = require('../app/Services/whatsapp-web/util');
-    const time = await value.GenerateRandomTime(10000, 50000);
-    return time;
-}
-temporizadorComSegundosRandomicos();
-//# sourceMappingURL=testePersistShippingCampaign.js.map

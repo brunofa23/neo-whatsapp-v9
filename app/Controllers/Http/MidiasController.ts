4847e1deb5375298d7101887fad2621e3161d99a
Bruno Favato <brunofa23@gmail.com>
2024-06-06 17:15:25 -0300

    rotas de audio

--- app/Controllers/Http/MidiasController.ts
+++ app/Controllers/Http/MidiasController.ts
@@ -2,17 +2,24 @@
 import Application from '@ioc:Adonis/Core/Application'
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
-
-
 const fs = require('fs-extra')
 const path = require('path')
 
 export default class MidiasController {
 
-  public async index({ }: HttpContextContract) {
-    console.log("Index Midias...")
+  public async midia({ response, params}: HttpContextContract) {
+    const filePath = `Medias/Customchats/${params.filename}`
+    console.log("Index Midias...", filePath)
+    return response.download(filePath)
   }
 
+
+  public async midiapath({ params}: HttpContextContract) {
+    console.log("Index Midias path...")
+    const fileName = params.filename
+    const baseUrl ='http://localhost:3334/api/midia'
+    return {url: `${baseUrl}/${fileName}`}
+  }
 
 
   public async storeMedia(media, fileName, folder) {
@@ -28,7 +35,9 @@ export default class MidiasController {
       await fs.ensureDir(path.dirname(filePath))
       fs.writeFileSync(filePath, buffer);
       console.log("ARQUIVO SALVO COM SUCESSO")
-      return filePath
+
+      return `Medias/${folder}/${fileNameFull}`
+
 
     } catch (error) {
       console.log("ERROR")

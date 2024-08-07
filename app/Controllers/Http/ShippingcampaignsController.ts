984e015b0823e19ad484005e2adfb3ffbb1a06f8
Bruno Favato <brunofa23@gmail.com>
2024-08-07 13:42:51 -0300

    ajustes

--- app/Controllers/Http/ShippingcampaignsController.ts
+++ app/Controllers/Http/ShippingcampaignsController.ts
@@ -596,6 +596,7 @@ export default class ShippingcampaignsController {
       subquery.select('*').from('chats').whereRaw('shippingcampaigns.id = chats.shippingcampaigns_id');
     }).orderBy('prioritysend', "desc")
 
+    console.log(query.toQuery())
     const shippingCampaign = await query.first()
     return shippingCampaign
 
--- app/Services/whatsapp-web/SendMessage.ts
+++ app/Services/whatsapp-web/SendMessage.ts
@@ -59,7 +59,7 @@ export default async (client: Client, agent: Agent) => {
     const maxLimitSendAgent = await maxLimitSendMessageAgent(agent.id)
     console.log(">>>>>>", agent.company_id)
     const shippingCampaign = await shippingcampaignsController.patientToSend(agent)
-    //console.log("PASSO 1>>>>", shippingCampaign)
+    console.log("PASSO 1>>>>", shippingCampaign?.id)
 
     let verifyChat
     let verifycontsend

1671ef1a30975833ac6d59ebc66b2773092b6f28
Bruno Favato <brunofa23@gmail.com>
2024-08-27 17:59:48 -0300

    AJUSTES

--- app/Services/whatsapp-web/AutomaticResponses.ts
+++ app/Services/whatsapp-web/AutomaticResponses.ts
@@ -1,7 +1,7 @@
 
 export default async (message: string) => {
 
-  if(message.toUpperCase().includes('BOM DIA'))
+  if(message.toUpperCase().includes('BOM DIA') || message.toUpperCase().includes('BOA TARDE')||message.toUpperCase().includes('BOA NOITE'))
     return "Bom dia, tudo bem? Sou a Iris, o chatbot do Neo Hospital de Olhos. Para qualquer esclarecimento ligue para 31-32350003. Obrigada!!"
   if(message.toUpperCase().includes('REAGENDAMENTO'))
     return "Gostaria de fazer um reagendamento? Para maiores esclarecimentos ligue para 31-32350003."
@@ -10,7 +10,7 @@ export default async (message: string) => {
   if(message.toUpperCase().includes('OBRIGADO') || message.toUpperCase().includes('OBRIGADA'))
     return "Imagina, nós que agradecemos!"
   if(message.toUpperCase().includes('TELEFONE ERRADO')||message.toUpperCase().includes('NUMERO ERRADO')||message.toUpperCase().includes('NÚMERO ERRADO') || message.toUpperCase().includes('NÃO SOU EU'))
-    return "Desculpe, acredito que houve algum erro em meu cadastro. Obridada!!"
+    return "Desculpe, acredito que houve algum erro em nosso cadastro, iremos corrigir. Obridada!!"
 
 
 }

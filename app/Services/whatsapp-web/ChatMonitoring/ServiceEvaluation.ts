f8c0756ac3f633af437ff152fa19cf38dadde455
Bruno Favato <brunofa23@gmail.com>
2024-08-27 15:18:36 -0300

    ajuste avaliação

--- app/Services/whatsapp-web/ChatMonitoring/ServiceEvaluation.ts
+++ app/Services/whatsapp-web/ChatMonitoring/ServiceEvaluation.ts
@@ -13,9 +13,13 @@ export default async (client: Client, message: Message, chat: Chat) => {
   }
   //PERGUNTA 1 - AVALIAÇÃO DE ATENDIMENTO
   if (chat.interaction_seq == 1) {
-    const notes = message.body.match(/\d+/g);
-    let invalidNote = notes.some(note => parseInt(note) > 10);
-    let invalidNoteNegative = notes.some(note => parseInt(note) < 0);
+    const notes = message.body.replace('1o','10').match(/\d+/g);
+    let invalidNote
+    let invalidNoteNegative
+    if (notes) {
+      invalidNote = notes.some(note => parseInt(note) > 10);
+      invalidNoteNegative = notes.some(note => parseInt(note) < 0);
+    }
     if (notes === null || notes.length == 0 || notes == undefined || invalidNote || invalidNoteNegative) {
       await stateTyping(message)//status de digitando...
       client.sendMessage(message.from, `Desculpe,😔 não consegui identificar sua nota. Por favor poderia responder uma nota entre 0 a 10?`)

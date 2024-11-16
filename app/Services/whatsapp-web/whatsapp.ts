57725da21ac2677c56926fac88c1ab395f37ebfd
Bruno Favato <brunofa23@gmail.com>
2024-11-16 20:02:04 -0300

    outros ajustes

--- app/Services/whatsapp-web/whatsapp.ts
+++ app/Services/whatsapp-web/whatsapp.ts
@@ -1,24 +1,16 @@
 import Agent from 'App/Models/Agent';
-//import SendMessage from 'App/Services/whatsapp-web/SendMessage'
+import Shippingcampaign from 'App/Models/Shippingcampaign';
 import ChatMonitoring from './ChatMonitoring/ChatMonitoring'
-//import ChatMonitoringAgentChat from './ChatMonitoring/ChatMonitoringAgentChat'
 import ChatMonitoringInternal from './ChatMonitoring/ChatMonitoringInternal'
 import SendMessageAgentDefault from './SendMessageAgentDefault';
-//import SendMessageInternal from './SendMessageInternal';
-import { ClearFolder, DateFormat, ExecutingSendMessage, GenerateRandomTime, RandomResponse, TimeSchedule, validAgent, ValidatePhone } from './util'
 import Customchat from 'App/Models/Customchat';
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 const qrcodeTerminal = require('qrcode-terminal');
 const qrcode = require('qrcode')
-//const path = require('path')
-//const folderPath = path.resolve(__dirname, "../../../");
-//let qrcodePath
-
 
 async function startAgentChat(_agent: Agent) {
   const agent = await Agent.findOrFail(_agent.id)
-
   if (!_agent) {
     console.log("CHATNAME INVÁLIDO - Verifique o .env Chatname está igual ao name tabela Agents")
     return
@@ -44,39 +36,28 @@ async function startAgentChat(_agent: Agent) {
   });
 
 
-  //console.log("passei no 1501 - startAgent", agent.name)
   clientChat.initialize();
   clientChat.on('loading_screen', (percent, message) => {
     console.log(`LOADING SCREEN: ${_agent.name}`, percent, message);
-    // agent.status = `Carregando: ${_agent.name} - ${percent} - ${message}`
-    // agent.save()
   });
 
 
   clientChat.on('qr', async (qr) => {
-
     agent.status = "Qrcode require"
     agent.statusconnected = false
     await agent.save()
-
     qrcode.toDataURL(qr, (err, url) => {
       if (err) {
         console.error('Ocorreu um erro ao gerar o URL de dados:', err);
         return;
       }
-      //console.log('URL de dados do código QR:', url);
       agent.qrcode = url
       agent.save()
     });
-
     qrcodeTerminal.generate(qr, { small: true });
-    // const folderPath = path.resolve(__dirname, "../../../");
-    // qrcodePath = path.join(folderPath, "/qrcode", `qrcode${agent.name}.png`)
-    // ClearFolder(qrcodePath)
+
   });
 
-
-  //console.log("passei no 1500 - startAgent")
   clientChat.on('authenticated', () => {
     console.log(`AUTHENTICATED ${agent.name}`);
     agent.status = 'Authentication'
@@ -90,19 +71,11 @@ async function startAgentChat(_agent: Agent) {
   });
 
   await clientChat.on('ready', async () => {
-    //ClearFolder(qrcodePath)
     console.log(`READY...${agent.name}`);
     const state = await clientChat.getState()
     console.log("State:", state)
     console.log("INFO:", await clientChat.info)
-
     await SendMessageAgentDefault(clientChat, agent)
-
-    // if (process.env.SELF_CONVERSATION?.toLocaleLowerCase() === "true") {
-    //   console.log("self_conversation", process.env.SELF_CONVERSATION)
-    //   await SendMessageInternal(clientChat)
-    // }
-
     agent.status = state
     agent.statusconnected = true
     agent.number_phone = clientChat.info.wid.user
@@ -111,7 +84,7 @@ async function startAgentChat(_agent: Agent) {
 
   });
 
-  clientChat.on('message_ack', async(msg, ack) => {
+  clientChat.on('message_ack', async (msg, ack) => {
     /*
         == ACK VALUES ==
         ACK_ERROR: -1
@@ -121,10 +94,10 @@ async function startAgentChat(_agent: Agent) {
         ACK_READ: 3
         ACK_PLAYED: 4
     */
-   const returnAck = await Customchat.query()
-   .where('message',msg.body)
-   .andWhere('cellphoneserialized', msg.to)
-   .update({ack:msg.ack})
+    const returnAck = await Customchat.query()
+      .where('message', msg.body)
+      .andWhere('cellphoneserialized', msg.to)
+      .update({ ack: msg.ack })
   });
 
   const chatMonitoring = new ChatMonitoring
@@ -134,17 +107,23 @@ async function startAgentChat(_agent: Agent) {
     const chatMonitoringInternal = new ChatMonitoringInternal
     await chatMonitoringInternal.monitoring(clientChat)
   }
-
-
-
   //************************************************ */
   clientChat.on('disconnected', async (reason) => {
-    console.log("EXECUTANDO DISCONECT")
-    console.log("REASON>>>", reason)
     agent.status = 'Disconnected'
     agent.statusconnected = false
     await agent.save()
-
+    await Shippingcampaign.create({
+      interaction_id: 3,
+      interaction_seq: 1,
+      message: `O agente ${agent.number_phone} foi desconectado!`,
+      cellphone: '31985228619',
+      reg: 1,
+      name: 'Bruno',
+      prioritysend: true
+    })
+    console.log("EXECUTANDO DISCONECT")
+    console.log("REASON>>>", reason)
+    return
   });
 
 
@@ -154,14 +133,6 @@ async function startAgentChat(_agent: Agent) {
     if (rejectCalls) await call.reject();
     await clientChat.sendMessage(call.from, `[${call.fromMe ? 'Outgoing' : 'Incoming'}] Olá tudo Bem? Sou uma atendente virtual e por isso não consigo receber chamadas. Desculpe!!☺️`);
   });
-
-
   return clientChat
-
 }
-
-
-
-
-
 module.exports = { startAgentChat }

81e23ec88f39547a6296d5b45f0b483eb4b42c7f
Bruno Favato <brunofa23@gmail.com>
2025-01-24 11:03:29 -0300

    envio isolado de mensagem

--- tests/functional/hello_world.spec.ts
+++ tests/functional/hello_world.spec.ts
@@ -7,37 +7,16 @@ import { getSchedulesApi, confirmOrCancelScheduleApi } from 'App/Services/reques
 import { ValidatePhone } from 'App/Services/whatsapp-web/util'
 import ResponsesController from './ResponsesController'
 import { DateTime } from 'luxon'
+import Agent from 'App/Models/Agent'
 import Response from 'App/Models/Response'
+import { startAgent, getWhatsAppClient, destroyAgent } from "App/Services/whatsapp-web/whatsappConnection"
+
 test('display welcome page', async ({ client }) => {
 
   //chmamar a API DO KLINGO
-  const formatMessage = (template, fields) => {
-    return template
-   .replace('{name_unit}', fields.name_unit)
-   .replace('{address_unit}', fields.address_unit || 'Endereço indisponível')
-   .replace('{medic}', fields.medic || 'Médico não informado')
-   .replace('{phone_unit}', fields.phone_unit || 'Contato indisponível')
-   .replace('{schedule}', fields.schedule)
-};
+  const agent = await Agent.query().where('id', 6).first()
+  //console.log(agent)
+  client = await startAgent(agent)
 
-  const chatOtherFields = JSON.parse(`{"address_unit":"Rua Álvares Maciel, 356 - Santa Efigênia - BH","medic":"ALINE TEIXEIRA GUIDINE","schedule":"2025-01-23 07:30","phone_unit":"(31) 3227-1000","name_unit":"BH (BAIRRO SANTA EFIGÊNIA) - CENTRO DE OFTALMOLOGIA BRASIL"}`)
-
-  //console.log(chatOtherFields.address_unit)
-
-  const response1schedule = await Response.query()
-    .select('message')
-    .where('local', 'response1schedule')
-    .andWhere('inactive', false)
-    .first();
-
-  const defaultMessage = "teste"//`Muito obrigada 😀, seu agendamento foi confirmado, o endereço da sua consulta é {chat.shippingcampaign.address}. Esperamos por você. Ótimo dia. Lembrando que para qualquer dúvida, estamos disponíveis pelo whatsapp {chat.shippingcampaign.phone_unit}.`;
-
-
-  const response1message = response1schedule
-  ? formatMessage(response1schedule.message, chatOtherFields)
-  : defaultMessage;
-
-
-  console.log(response1message)
 
 })

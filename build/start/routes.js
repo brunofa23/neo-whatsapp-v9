40e3fcf52fcf80235bcd57bb49576ffa95e30502
Bruno Favato <brunofa23@gmail.com>
2025-05-16 14:51:51 -0300

    com build

--- build/start/routes.js
+++ build/start/routes.js
@@ -6,7 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const Route_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Route"));
 const PersistShippingcampaign_1 = __importDefault(global[Symbol.for('ioc.use')]("App/Services/whatsapp-web/PersistShippingcampaign"));
 const events_1 = require("./events");
-console.log("***CHAT BOT V-125***04/10/2024", process.env.SERVER);
+console.log("***CHAT BOT V-126***16/05/2025", process.env.SERVER);
 function operacaoAssincrona(callback) {
     console.log("ENTREI PASSO 1", callback);
     if (process.env.SERVER === 'true') {

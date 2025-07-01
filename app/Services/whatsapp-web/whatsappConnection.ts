deaa8566fe692c4c78c44d16c0ae261b669d39c3
Bruno Favato <brunofa23@gmail.com>
2025-07-01 17:06:49 -0300

    utilizando chrome ao inves do chromium

--- app/Services/whatsapp-web/whatsappConnection.ts
+++ app/Services/whatsapp-web/whatsappConnection.ts
@@ -40,7 +40,8 @@ async function startAgent(_agent: Agent) {
     authStrategy: new LocalAuth({ clientId: _agent.id, dataPath: Application.tmpPath('/sessions') }),
     //authStrategy: new LocalAuth({ clientId: _agent.id }),
     puppeteer: {
-      executablePath: '/usr/bin/chromium-browser',
+      //executablePath: '/usr/bin/chromium-browser',
+      executablePath: '/usr/bin/google-chrome',
       args: ['--no-sandbox',
         '--disable-setuid-sandbox',
         '--disable-dev-shm-usage',

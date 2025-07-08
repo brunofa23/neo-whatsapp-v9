781788deeb24b30560b952ff7f3466c8f337722f
Bruno Favato <brunofa23@gmail.com>
2025-07-08 09:15:00 -0300

    v13

--- build/app/Services/whatsapp-web/whatsapp.js
+++ build/app/Services/whatsapp-web/whatsapp.js
@@ -23,7 +23,7 @@ async function startAgentChat(_agent) {
     const clientChat = new Client({
         authStrategy: new LocalAuth({ clientId: _agent.id, dataPath: Application_1.default.tmpPath('/sessions') }),
         puppeteer: {
-            executablePath: '/usr/bin/google-chrome',
+            executablePath: '/snap/bin/chromium',
             args: ['--no-sandbox',
                 '--disable-setuid-sandbox',
                 '--disable-dev-shm-usage',

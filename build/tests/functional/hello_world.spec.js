8453f0e6d108bca29751c912d555f03ef2048abf
Bruno Favato <brunofa23@gmail.com>
2025-09-02 14:46:49 -0300

    atualizacao api

--- build/tests/functional/hello_world.spec.js
+++ build/tests/functional/hello_world.spec.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const runner_1 = require("@japa/runner");
 const IdentifyAnswer_1 = global[Symbol.for('ioc.use')]("App/Services/whatsapp-web/IdentifyAnswer");
 (0, runner_1.test)('display welcome page', async ({ client }) => {
-    const answer = await (0, IdentifyAnswer_1.interpretAnswer)("bom dia, 2");
+    const answer = await (0, IdentifyAnswer_1.interpretAnswer)("bom dia 1");
     console.log("RESPOSTA>>>>", answer.code);
 });
 //# sourceMappingURL=hello_world.spec.js.map

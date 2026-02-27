efffe6fd22eef1f729f84eab5d10f3dc830430ac
Bruno Favato <brunofa23@gmail.com>
2026-02-27 18:58:09 +0000

    chore: build para deploy a partir da main-officialApi

--- build/app/Controllers/Http/MidiasController.js
+++ build/app/Controllers/Http/MidiasController.js
@@ -4,13 +4,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const Application_1 = __importDefault(global[Symbol.for('ioc.use')]("Adonis/Core/Application"));
+const fs_1 = require("fs");
 const fs = require('fs-extra');
-const path = require('path');
 class MidiasController {
     async midia({ response, params }) {
         const fileName = params.filename;
         const filePath = Application_1.default.makePath(`Medias/Customchats/${fileName}`);
-        console.log('MidiasController.midia filename:', fileName);
+        console.log('MidiasController.midia fileName:', fileName);
         console.log('MidiasController.midia filePath:', filePath);
         if (!fs.existsSync(filePath)) {
             console.log('MidiasController.midia -> arquivo não encontrado');
@@ -20,12 +20,13 @@ class MidiasController {
                 filePath,
             });
         }
-        return response.download(filePath);
+        response.header('Content-Type', 'audio/ogg');
+        response.header('Accept-Ranges', 'bytes');
+        return response.stream((0, fs_1.createReadStream)(filePath));
     }
     async midiapath({ params }) {
         const fileName = params.filename;
-        const url = `/api/midia/${fileName}`;
-        return { url };
+        return { url: `/api/midia/${fileName}` };
     }
 }
 exports.default = MidiasController;
